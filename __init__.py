ef9cccdf47e1330e32d0b12bc5e754a0a820d55f
nouxinf <180769756+nouxinf@users.noreply.github.com>
2026-07-27 20:40:47 +0100

    add debugging stuff. preparing for fetching osm nominatim geocoding.

--- __init__.py
+++ __init__.py
@@ -4,7 +4,41 @@ from breakout_bme280 import BreakoutBME280
 from lsm6ds3 import LSM6DS3, NORMAL_MODE_104HZ
 from breakout_ltr559 import BreakoutLTR559
 import json
+import network
+import urequests as requests
 
+# COLOR PALLETTE
+BACKGROUND_COLOR = color.rgb(59, 145, 173)
+BLACK = color.black
+WHITE = color.white
+
+screen.pen = BLACK
+screen.clear()
+
+messages = []
+
+
+def show_status(message):
+    global messages
+    screen.pen = BLACK
+    screen.clear()
+    screen.pen = WHITE
+
+    screen.text("Loading app...", 10, 10)
+
+    for index, msg in enumerate(messages):
+        screen.text(msg, 10, 20 + (index * 10))
+
+    new_y = 20 + (len(messages) * 10)
+    screen.text(message, 10, new_y)
+
+    messages.append(message)
+    display.update()
+
+
+show_status("Loading sensor...")
+
+# try to initialise the multisensor
 try:
     i2c = I2C()
     bme = BreakoutBME280(i2c)
@@ -17,10 +51,100 @@ else:
     last_read = 0
     readings = bme.read()
 
+if no_multisensor:
+    show_status("No multisensor found")
+else:
+    show_status("Multisensor found")
+show_status("Finding Wi-Fi details...")
+try:
+    from secrets import WIFI_SSID, WIFI_PASSWORD
+except ImportError:
+    show_status(
+        "Couldn't find Wi-Fi details, write them in secrets.py or else you won't be able to use internet"
+    )
+    raise
 
-# COLOR PALLETTE
-BACKGROUND_COLOR = color.rgb(59, 145, 173)
-WHITE = color.white
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+show_status("Connecting to Wi-Fi...")
+if not wlan.isconnected():
+    print("Connecting to Wi-Fi...")
+    wlan.connect(WIFI_SSID, WIFI_PASSWORD)
+    # Wait for connection
+    while not wlan.isconnected():
+        time.sleep(1)
+
+print("Connected to Wi-Fi:", wlan.ipconfig("addr4"))
+show_status("Connected")
+show_status("Loading options.json")
+try:
+    with open("options.json") as f:
+        options = json.load(f)
+except Exception as e:
+    show_status("Failed to load options.json!!")
+    raise SystemExit("Failed to load options.json!!")
+# fetching stuff
+url = "https://nominatim.openstreetmap.org/reverse"
+headers = {"User-Agent": "Weatherstation on the Tufty 2350"}
+
+try:
+    """
+    print(f"Making GET request to {url}...")
+                                       response = requests.get(url, headers=headers)
+
+                                       if response.status_code == 200:
+                                                                       data = response.json()
+                                                                       print(f"Success! Repo: {data['full_name']}")
+                                                                       print(f"Description: {data['description']}")
+                                                                       print(f"Stars: {data['stargazers_count']}")
+                                       else:
+                                                                       print(f"Request failed with status code: {response.status_code}")
+                                                                       print(response.text)
+
+                                       response.close()
+    """
+    show_status("Making GET requests...")
+    MIN_VALUE = -90.0
+    MAX_VALUE = 90.0
+
+    try:
+        locations = options["locations"]
+
+        if not isinstance(locations, list) or len(locations) < 1:
+            raise ValueError("Locations must be a non-empty list")
+
+        for idx, entry in enumerate(locations):
+            # must be a list/tuple of exactly 2 values
+            if not isinstance(entry, (list, tuple)) or len(entry) != 2:
+                raise ValueError(f"Entry {idx} must be an array of exactly 2 values")
+
+            val1, val2 = entry
+
+            # must be numbers within the latlon range
+            if not (isinstance(val1, (int, float)) and isinstance(val2, (int, float))):
+                raise ValueError(f"Entry {idx} contains non-numeric values")
+
+            if not (MIN_VALUE <= val1 <= MAX_VALUE and MIN_VALUE <= val2 <= MAX_VALUE):
+                raise ValueError(
+                    f"Entry {idx} values {entry} are outside the range [{MIN_VALUE}, {MAX_VALUE}]"
+                )
+
+        # if we reach here without exceptions data is valid
+        print(f"Valid locations: {locations}")
+        for i in locations:
+            response = requests.get(
+                f"{url}?lat={i[0]}&lon={i[1]}&format=json&addressdetails=1"
+            )
+            if response.statuscode == 200:
+                data = response.json()
+                print(data)
+
+    except (KeyError, ValueError) as e:
+        show_status("Failed to get locations")
+        raise SystemExit
+except Exception as e:
+    print("An error occurred:", e)
+
 
 FONT = font.load("/system/assets/fonts/MonaSans-Medium.af")
 screen.font = FONT
@@ -28,9 +152,6 @@ screen.font = FONT
 sprites = SpriteSheet(
     f"assets/spritesheet.png", 30, 1
 )  # remember to update column count
-
-with open("options.json") as f:
-    options = json.load(f)
 
 
 def temp_to_sprite(temp, low=-20, high=45, step=5, num_sprites=13):
@@ -53,50 +174,100 @@ def pres_to_sprite(pres, low=950, high=1050, step=14.29, num_sprites=7, start_co
     return start_col + index
 
 
+current_screen = 0
+screens = ["sensor"] + options.get("locations")
+print(screens)
+
+prev_down = False
+prev_up = False
+
+
+def move_current_screen():
+    global current_screen, prev_down, prev_up
+
+    down_now = badge.pressed(BUTTON_DOWN)
+    up_now = badge.pressed(BUTTON_UP)
+
+    if down_now and not prev_down:
+        current_screen = (current_screen + 1) % len(screens)
+    if up_now and not prev_up:
+        current_screen = (current_screen - 1) % len(screens)
+
+    prev_down = down_now
+    prev_up = up_now
+    # print(current_screen)
+    # print(screens)
+
+
 def update():
-    if not no_multisensor:
-        global last_read, readings
-        now = time.ticks_ms()
-        if time.ticks_diff(now, last_read) > 100:  # refresh ten times a second
-            readings = bme.read()
-            last_read = now
-            print(readings)
+    move_current_screen()
+    if current_screen == 0:
+        if not no_multisensor:
+            global last_read, readings
+            now = time.ticks_ms()
+            if (
+                time.ticks_diff(now, last_read) > 100
+            ):  # refresh ten times a second, going faster makes it freeze
+                readings = bme.read()
+                last_read = now
+            # print(readings)
 
-        temp = round(readings[0], 1)
-        humidity = round(readings[2], 0)
-        pressure = round(readings[1], 2) / 100
+            temp = round(readings[0], 1)
+            humidity = round(readings[2], 0)
+            pressure = round(readings[1], 2) / 100
 
-    # Draw UI
+        # Draw UI
 
-    screen.pen = BACKGROUND_COLOR
-    screen.clear()
-    screen.pen = color.white
-    biggest_rectangle = shape.rounded_rectangle(5, 5, 150, 110, 10)
-    smaller_rectangle = shape.rounded_rectangle(7, 7, 146, 106, 10)
-    screen.shape(biggest_rectangle)
-    screen.pen = BACKGROUND_COLOR
-    screen.shape(smaller_rectangle)
-    screen.pen = WHITE
-    screen.text("Local sensor data", 10, 10, 15)
+        screen.pen = BACKGROUND_COLOR
+        screen.clear()
+        screen.pen = color.white
+        biggest_rectangle = shape.rounded_rectangle(5, 5, 150, 110, 10)
+        smaller_rectangle = shape.rounded_rectangle(7, 7, 146, 106, 10)
+        screen.shape(biggest_rectangle)
+        screen.pen = BACKGROUND_COLOR
+        screen.shape(smaller_rectangle)
+        screen.pen = WHITE
+        screen.text("Local sensor data", 10, 10, 15)
 
-    # Display info
+        # Display info
 
-    temp_unit = options.get("tempmeasurement", "unknown")
-    if not no_multisensor:
-        if temp_unit == "F":
-            screen.text(f"{((temp * 1.8) + 32):.1f}°F", 25, 25, 20)
-        elif temp_unit == "K":
-            screen.text(f"{(temp + 273.15):.1f}°K", 25, 25, 20)
+        temp_unit = options.get("tempmeasurement", "unknown")
+        if not no_multisensor:
+            if temp_unit == "F":
+                screen.text(f"{((temp * 1.8) + 32):.1f}°F", 25, 25, 20)
+            elif temp_unit == "K":
+                screen.text(f"{(temp + 273.15):.1f}°K", 25, 25, 20)
+            else:
+                screen.text(f"{temp}°C", 25, 25, 20)
+            screen.text(f"{humidity:.1f}%", 25, 45, 20)
+            screen.text(f"{pressure:.2f}hPa", 25, 68, 20)
+            screen.blit(sprites.sprite(temp_to_sprite(temp), 0), vec2(7, 28))
+            screen.blit(sprites.sprite(hum_to_sprite(humidity), 0), vec2(7, 50))
+            screen.blit(sprites.sprite(pres_to_sprite(pressure), 0), vec2(7, 72))
         else:
-            screen.text(f"{temp}°C", 25, 25, 20)
-        screen.text(f"{humidity:.1f}%", 25, 45, 20)
-        screen.text(f"{pressure:.2f}hPa", 25, 68, 20)
-        screen.blit(sprites.sprite(temp_to_sprite(temp), 0), vec2(7, 28))
-        screen.blit(sprites.sprite(hum_to_sprite(humidity), 0), vec2(7, 50))
-        screen.blit(sprites.sprite(pres_to_sprite(pressure), 0), vec2(7, 72))
+            screen.text("No sensor detected", 10, 25, 15)
+    elif current_screen != 0 and current_screen <= len(screens):
+        screen.pen = BACKGROUND_COLOR
+        screen.clear()
+        screen.pen = color.white
+        biggest_rectangle = shape.rounded_rectangle(5, 5, 150, 110, 10)
+        smaller_rectangle = shape.rounded_rectangle(7, 7, 146, 106, 10)
+        screen.shape(biggest_rectangle)
+        screen.pen = BACKGROUND_COLOR
+        screen.shape(smaller_rectangle)
+        screen.pen = WHITE
+        screen.text(f"Placeholder {options.get("locations")}", 10, 10, 15)
     else:
-        screen.text("No sensor detected", 10, 25, 15)
-    # Draw sprites
+        screen.pen = BACKGROUND_COLOR
+        screen.clear()
+        screen.pen = color.white
+        biggest_rectangle = shape.rounded_rectangle(5, 5, 150, 110, 10)
+        smaller_rectangle = shape.rounded_rectangle(7, 7, 146, 106, 10)
+        screen.shape(biggest_rectangle)
+        screen.pen = BACKGROUND_COLOR
+        screen.shape(smaller_rectangle)
+        screen.pen = WHITE
+        screen.text("Invalid screen", 10, 10, 15)
 
 
 run(update)
