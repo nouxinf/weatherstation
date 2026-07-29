31c57c64db4f1cba8e2d95c24e00b64630924245
nouxinf <180769756+nouxinf@users.noreply.github.com>
2026-07-29 13:10:27 +0100

    woops forgot to stage

--- __init__.py
+++ __init__.py
@@ -196,7 +196,7 @@ except Exception as e:
     print("An error occurred:", e)
 
 
-FONT = font.load("/system/assets/fonts/MonaSans-Medium.af")
+VECTOR_FONT = font.load("/system/assets/fonts/MonaSans-Medium.af")
 screen.font = FONT
 
 sprites = SpriteSheet(
